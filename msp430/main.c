650f82c2a8e219eb4366432dc21f0f3c54e8199e
Selikem Kwadzovia <skwadzov@sfu.ca>
2022-01-02 14:13:38 -0800

    Added simple blink modes
    Attached to button on P2.3 of MSP430FR2355

--- msp430/main.c
+++ msp430/main.c
@@ -11,11 +11,22 @@ uint8_t getPortLow(unsigned const charliePin[3]);
 uint8_t getPortHigh(unsigned const charliePin[3]);
 void setLED(unsigned const charliePortLUT[][3],uint8_t ledPIN,size_t MAX_SIZE);
 
+uint16_t delaySize = 300;
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	//P2.3 Button Setup
+	P2DIR &= ~BIT3;
+	P2REN |= BIT3;
+	P2OUT |= BIT3;
+	P2IES |= BIT3;              // Interrupt on high to low
+	P2IE |= BIT3;
+
 	PM5CTL0 &= ~LOCKLPM5;       // Exit low power mode
+	__enable_interrupt();       // Enable global interrupts
+
 
 	unsigned const charliePortLUT[][3] = {
       {0x3,0x2,0x1},
@@ -31,6 +42,7 @@ int main(void)
 	volatile uint8_t infLoop = 1;
 	uint16_t delayIndex = 0;
 	uint8_t ledSelect = 0;
+
 	while(infLoop){
 	    //Range check on selected LED
 	    if(ledSelect > MAX_ROWS_CHARLIE_LUT-1){
@@ -42,7 +54,7 @@ int main(void)
         ledSelect++;
 
 	    //Short Delay
-	    for(delayIndex = 0;delayIndex < 300;delayIndex++){}
+	    for(delayIndex = 0;delayIndex < delaySize;delayIndex++){}
 	}
 	return 0;
 }
@@ -75,4 +87,18 @@ void setLED(unsigned const charliePortLUT[][3] ,uint8_t ledPIN,size_t MAX_SIZE){
         P6OUT |= getPortHigh(charliePortLUT[ledPIN]);
     }
 
+}
+
+#pragma vector = PORT2_VECTOR
+__interrupt void  PORT2_ISR(void){
+    __disable_interrupt();
+    P2IFG = 0;
+
+    if(delaySize > 40000){
+        delaySize = 300;
+    } else {
+        delaySize += 20000;
+    }
+
+    __enable_interrupt();
 }
